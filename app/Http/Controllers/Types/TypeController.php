a6cc556a338a310259c2851c4fae610b4eb48858
rama3awwad <rama.3awwad11@gmail.com>
2024-05-05 12:41:43 +0300

    update all

---
+++ app/Http/Controllers/Types/TypeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Types;
+
+use App\Http\Controllers\BaseController as BaseController;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends BaseController
+{
+    //show all types
+    public function index()
+    {
+        $types = Type::all();
+        return $this->sendResponse($types, 'Types retrieved successfully.');
+    }
+
+    //store type
+    public function store(Request $request)
+    {
+        $type = Type::create($request->all());
+        return $this->sendResponse($type, 'Type created successfully.');
+    }
+
+    //show type by id
+    public function show($id)
+    {
+        $type = Type::find($id);
+        if (is_null($type)) {
+            return $this->sendError('Type not found.');
+        }
+        return $this->sendResponse($type, 'Type retrieved successfully.');
+    }
+
+    //update type
+    public function update(Request $request, $id)
+    {
+        $type = Type::find($id);
+        if (is_null($type)) {
+            return $this->sendError('Type not found.');
+        }
+        $type->update($request->all());
+        return $this->sendResponse($type, 'Type updated successfully.');
+    }
+
+    //delete type
+    public function destroy($id)
+    {
+        $type = Type::find($id);
+        if (is_null($type)) {
+            return $this->sendError('Type not found.');
+        }
+        $type->delete();
+        return $this->sendResponse($id, 'Type deleted successfully.');
+    }
+}
