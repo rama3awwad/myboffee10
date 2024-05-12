f43b2ca52fc9d92186ce2f1757135042d35fb327
rama3awwad <rama.3awwad11@gmail.com>
2024-05-12 13:16:32 +0300

    update all

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'user_name',
         'email',
         'password',
-        'my_coins',
+        'my_points',
         'age',
         'image',
         'gendre_id'
