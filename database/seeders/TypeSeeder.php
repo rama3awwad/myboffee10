e20c4cf4c9f52ac5d52353d5e42a017b4b3c588e
rama3awwad <143185600+rama3awwad@users.noreply.github.com>
2024-06-13 17:31:25 +0300

    Add files via upload

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            ['name' => 'Novels'],
+            ['name' => 'Islamic'],
+            ['name' => 'Children'],
+            ['name' => 'Scientific'],
+            ['name' => 'Horror'],
+            ['name' => 'Human Development'],
+        ];
+
+        foreach ($types as $type) {
+<<<<<<< HEAD
+
+            $existingType = Type::where('name', $type['name'])->first();
+
+            if ($existingType) {
+                continue;
+            }
+            Type::create($type);
+        }
+=======
+   $existingType = Type::where('name', $type['name'])->first();
+
+            if ($existingType) {
+                continue;
+                    }
+>>>>>>> 8a2f3084e51edad2d0f6a1051349c8ce458ba4d1
+    }
+
+
+}
