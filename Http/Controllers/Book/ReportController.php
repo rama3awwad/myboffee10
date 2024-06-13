6135d36b15f9e07418f287c9a6432086f62bc80f
rama3awwad <143185600+rama3awwad@users.noreply.github.com>
2024-06-13 17:14:23 +0300

    Add files via upload

---
+++ Http/Controllers/Book/ReportController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Book;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ReportRequest;
+use App\Models\Report;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\Facades\Auth;
+
+
+class ReportController extends Controller
+{
+    //show all reports
+    public function index(): JsonResponse
+    {
+        $reports = Report::all();
+        return $this->sendResponse($reports, 'Reports retrieved successfully.');
+    }
+
+
+    // Add a report
+    public function store(ReportRequest $request, $bookId): JsonResponse
+    {
+        $request->validated();
+        $user = Auth::user();
+        $report = Report::create ([
+            'user_id'=>$request->user()->id,
+            'book_id'=>$request->$bookId,
+            'body'=>$request->body,
+        ]);
+
+        return $this->sendResponse($report, 'Report created successfully.');
+    }
+
+    //show report by its id for admin
+    public function show($id): JsonResponse
+    {
+        $report = Report::find($id);
+
+        if (is_null($report)) {
+            return $this->sendError('Report not found');
+        }
+
+        return $this->sendResponse($report, 'Report retrieved successfully');
+    }
+
+    //show my reports
+    public function showUserReports(): JsonResponse
+    {
+        $user = Auth::user();
+        $reports = $user->reports();
+
+        return $this->sendResponse($reports, 'User\'s reports');
+    }
+
+    // Show book reports
+    public function showReportsByBookId($bookId): JsonResponse
+    {
+        $reports = Report::where('book_id', $bookId)->get();
+
+        return $this->sendResponse($reports, 'Reports fetched successfully');
+    }
+
+    // Remove a report
+    public function removeReport($reportId): JsonResponse
+    {
+        $report = Report::findOrFail($reportId);
+        $report->delete();
+
+        return $this->sendResponse(null, 'Report removed successfully');
+    }
+
+    // Delete my reports
+    public function deleteAllUserReports(): JsonResponse
+    {
+        $user = Auth::user();
+        $user->reports()->detach();
+
+        return $this->sendResponse(null, 'All reports removed successfully');
+    }
+
+
+}
---
+++ Http/Controllers/Post/PostController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\BaseController;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends BaseController
+{
+    public function index()
+    {
+        $post = Post::all();
+        return $this->sendResponse($post, 'Posts retrieved successfully');
+    }
+
+    //show user's posts
+    public function show($userId){
+
+       // $user = Auth::user();
+        $user = User::where('id', $userId)->first();
+        $posts = $user->posts;
+        return $this->sendResponse($posts, 'user\'s posts retrieved successfully');
+    }
+
+    //show my posts
+    public function showMyPosts(){
+
+        $user_id = Auth::user()->id;
+        $user = User::where('id', $user_id)->first();
+        $posts = $user->posts;
+        return $this->sendResponse($posts, 'Your posts retrieved successfully');
+    }
+
+    public function create(Request $request){
+
+        $request->validate([
+            'body'=> 'required',
+
+        ]);
+        $input=$request->all();
+
+        $user = auth()->user()->user_name;
+        $user_id = Auth::user()->id;
+        $post = Post::create([
+
+            'user_id' => $user_id,
+            'user_name' => $user,
+            'body' => $request->body,
+            'likes_count'=>$request->likes_count ?? 0
+        ]);
+
+        return $this->sendResponse($post, 'Post created successfully.');
+    }
+  /* public static function updateLikesCount($postId)
+    {
+       // $posts = Post::all();
+       $user = Auth::user();
+       $posts = $user->favoritePosts()->where('post_id' , $postId)->exists();
+        foreach ($posts as $post) {
+            $post = Post::find($postId);
+            if($post){
+            $totalLikes = $post->favoritesPosts()->count();
+            $post->update(['likes_count' => $totalLikes]);
+        }}
+    }
+*/
+    public function update(Request $request, Post $id ){
+
+        $post = Post::find($id);
+        if (is_null($post)) {
+            return $this->sendError('Post not found');}
+        else{
+
+            $request->validate([
+                'body'=> 'required',
+            ]);
+            $input=$request->all();
+
+            $user_id = Auth::user()->id;
+            $post = Post::make([
+
+                'user_id' => $user_id,
+                'body' => $request->body,
+            ]);
+
+            return $this->sendResponse($post, 'Post updated successfully');
+
+        }
+    }
+
+    //show post
+    public function showP($post_id){
+
+        $post = Post::find($post_id);
+
+        if (is_null($post)) {
+            return $this->sendError('Post not found');
+        }
+
+        return $this->sendResponse($post, 'Post retrieved successfully');
+
+    }
+    public function delete(Post $id)
+    {
+        $post = Post::find($id);
+        if (is_null($post)) {
+            return $this->sendError('Post not found');
+        }
+
+        $post->each->delete();
+
+        return $this->sendResponse(null, 'Post deleted successfully');
+    }
+
+}
