a9326bbfd0d5fb95cf20ca01e566503aa63aba7d
rama3awwad <rama.3awwad11@gmail.com>
2024-08-16 11:58:20 +0300

    update all

--- database/migrations/2024_08_13_203642_create_user_devices_table.php
+++ database/migrations/2024_08_13_203642_create_user_devices_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('user_devices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->string('fcm_token')->unique();
+            $table->string('fcm_token');
             $table->timestamps();
         });
     }
